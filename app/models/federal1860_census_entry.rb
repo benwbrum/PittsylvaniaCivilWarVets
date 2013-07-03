c1ac7a12d635e270f8794a841f88b450888f68b3
Ben W. Brumfield <benwbrum@gmail.com>
2013-07-03 11:12:22 -0500

    Display households and navigation for 1860 census records

--- app/models/federal1860_census_entry.rb
+++ app/models/federal1860_census_entry.rb
@@ -5,11 +5,33 @@ class Federal1860CensusEntry < ActiveRecord::Base
   self.primary_key = 'id'
   attr_accessible :age, :district, :family_number, :family_structure, :first_name, :found_in_both, :house_number, :id, :illiterate, :last_name, :ldf_vet_or_family, :married_within_year, :member_of_veteran_family, :member_of_veteran_household, :middle_initial, :notes, :occupation, :other, :page_number, :personal_estate_owned, :place_of_birth, :post_office, :race, :real_estate_owned, :school_within_year, :sex, :slaveholder_1860, :slaveholding_vet_household_1860, :veteran, :veteran_or_veteran_family, :war_experience, :written_page_number
   
+  has_one :soldier
+  
   SUPPRESSED_ATTRIBUTES = [
     "",
     "id"
   ]
 
+
+  def household
+    Federal1860CensusEntry.where(:district => self.district, :house_number => self.house_number).order(:id)
+  end
+
+  def head_of_next_household
+    Federal1860CensusEntry.where(:district => self.district, :house_number => self.house_number.to_i + 1).order(:id).first
+  end
+
+  def head_of_previous_household
+    Federal1860CensusEntry.where(:district => self.district, :house_number => self.house_number.to_i - 1).order(:id).first
+  end
+  
+  def next_entry
+    Federal1860CensusEntry.where(:id => self.id+1).first
+  end
+
+  def previous_entry
+    Federal1860CensusEntry.where(:id => self.id-1).first
+  end
 
   def self.display_year
     '1860'
